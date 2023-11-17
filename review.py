24dc903092a0d19ca0b8e20223ed88de3f334bc0
cher <cher18168402859@163.com>
2023-11-17 09:47:09 +0800

    update

--- review.py
+++ review.py
@@ -1,4 +1,18 @@
 import psycopg2
 import json
+import rich.progress
 
-file_path = "/home/yc/dataset/amazon/meta_Sports_and_Outdoors.json"
+file_path = "/home/yc/dataset/amazon/Sports_and_Outdoors.json"
+
+print("Reading Data ...")
+data = []
+with open(file_path, 'r') as f:
+    for line in rich.progress.track(f.readlines()):
+        data.append(json.loads(line))
+
+print("Reading Done")
+print("Number of rows: {}".format(len(data)))
+benchmark = len(data[0].keys())
+for item in rich.progress.track(data):
+    if not len(item.keys()) == benchmark:
+        print("False")
