10b5ccbd2bb6273fc3d7f9ece8f3afb8d04c753d
cher <cher18168402859@163.com>
2023-11-17 09:28:20 +0800

    update

--- metadata.py
+++ metadata.py
@@ -2,9 +2,12 @@ import psycopg2
 import json
 
 file_path = "/home/yc/dataset/amazon/Sports_and_Outdoors.json"
+
 print("Reading Data ...")
+data = []
 with open(file_path, 'r') as f:
-    data = json.load(f)
+    for line in f.readlines():
+        data.append(json.loads(line))
 
 print(len(data))
 print(data[0].keys())
