8fa9a555b207575018b72cd92e43a0e865a3e596
cher <cher18168402859@163.com>
2023-11-17 14:28:05 +0800

    update

--- metadata.py
+++ metadata.py
@@ -1,7 +1,7 @@
 import psycopg2
 import json
 import rich.progress
-from utils import sanitize_string
+from utils import sanitize_string, isfloat
 
 file_path = "/home/yc/dataset/amazon/meta_Sports_and_Outdoors.json"
 
@@ -26,7 +26,8 @@ print("Creating Tables ...")
 commands = (
     """
     DROP TABLE IF EXISTS metadata;
-    """,
+    """
+    ,
     """
     CREATE TABLE metadata (
       asin VARCHAR(255),
@@ -35,70 +36,84 @@ commands = (
       main_cat TEXT,
       price FLOAT
     );
-    """,
     """
-    DROP TABLE IF EXISTS category;
-    """,
-    """
-    CREATE TABLE category (
-      cate_id INTEGER,
-      category TEXT
-    );
-    """,
-    """
-    DROP TABLE IF EXISTS meta_cate;
-    """,
-    """
-    CREATE TABLE meta_cate (
-      asin VARCHAR(255),
-      cate_id INTEGER
-    );
-    """,
-    """
-    DROP TABLE IF EXISTS also_buy;
-    """,
-    """
-    CREATE TABLE also_buy (
-      asin VARCHAR(255),
-      buy_asin VARCHAR(255)
-    );
-    """,
-    """
-    DROP TABLE IF EXISTS also_view;
-    """,
-    """
-    CREATE TABLE also_view (
-      asin VARCHAR(255),
-      view_asin VARCHAR(255)
-    );
-    """,
-    """
-    DROP TABLE IF EXISTS feature;
-    """,
-    """
-    CREATE TABLE feature (
-      asin VARCHAR(255),
-      feature TEXT
-    );
-    """,
-    """
-    DROP TABLE IF EXISTS description;
-    """,
-    """
-    CREATE TABLE description (
-      asin VARCHAR(255),
-      description TEXT
-    );
-    """,
-    """
-    DROP TABLE IF EXISTS rank;
-    """,
-    """
-    CREATE TABLE rank (
-      asin VARCHAR(255),
-      rank TEXT
-    );
-    """
+    # ,
+    # """
+    # DROP TABLE IF EXISTS category;
+    # """
+    # ,
+    # """
+    # CREATE TABLE category (
+    #   cate_id INTEGER,
+    #   category TEXT
+    # );
+    # """
+    # ,
+    # """
+    # DROP TABLE IF EXISTS meta_cate;
+    # """
+    # ,
+    # """
+    # CREATE TABLE meta_cate (
+    #   asin VARCHAR(255),
+    #   cate_id INTEGER
+    # );
+    # """
+    # ,
+    # """
+    # DROP TABLE IF EXISTS also_buy;
+    # """
+    # ,
+    # """
+    # CREATE TABLE also_buy (
+    #   asin VARCHAR(255),
+    #   buy_asin VARCHAR(255)
+    # );
+    # """
+    # ,
+    # """
+    # DROP TABLE IF EXISTS also_view;
+    # """
+    # ,
+    # """
+    # CREATE TABLE also_view (
+    #   asin VARCHAR(255),
+    #   view_asin VARCHAR(255)
+    # );
+    # """
+    # ,
+    # """
+    # DROP TABLE IF EXISTS feature;
+    # """
+    # ,
+    # """
+    # CREATE TABLE feature (
+    #   asin VARCHAR(255),
+    #   feature TEXT
+    # );
+    # """
+    # ,
+    # """
+    # DROP TABLE IF EXISTS description;
+    # """
+    # ,
+    # """
+    # CREATE TABLE description (
+    #   asin VARCHAR(255),
+    #   description TEXT
+    # );
+    # """
+    # ,
+    # """
+    # DROP TABLE IF EXISTS rank;
+    # """
+    # ,
+    # """
+    # CREATE TABLE rank (
+    #   asin VARCHAR(255),
+    #   rank TEXT
+    # );
+    # """
 )
 for command in commands:
     cur.execute(command)
@@ -119,7 +134,7 @@ with open(file_path, 'r') as f:
             "rank": raw_data["rank"] if isinstance(raw_data["rank"], list) else [raw_data["rank"]],
             "also_view": raw_data["also_view"],
             "main_cat": raw_data["main_cat"],
-            "price": float(raw_data["price"][1:]) if raw_data["price"][1:].isdigit() else None,
+            "price": float(raw_data["price"][1:]) if isfloat(raw_data["price"][1:]) else None,
             "asin": raw_data["asin"],
         })
 print("Reading Done")
@@ -138,61 +153,61 @@ for item in rich.progress.track(data, description="Inserting Data ..."):
               sanitize_string(item["main_cat"]),
               item["price"])
     )
-    # Insert into also_buy
-    for buy_asin in item["also_buy"]:
-        cur.execute(
-            """
-            INSERT INTO also_buy
-            VALUES (%s, %s)
-            """, (item["asin"], buy_asin)
-        )
-    # Insert into also_view
-    for view_asin in item["also_view"]:
-        cur.execute(
-            """
-            INSERT INTO also_view
-            VALUES (%s, %s)
-            """, (item["asin"], view_asin)
-        )
-    # Insert into category and meta_cate
-    for category in item["category"]:
-        if category not in categories:
-            cur.execute(
-                """
-                INSERT INTO category
-                VALUES (%s, %s)
-                """, (len(category) + 1, sanitize_string(category))
-            )
-            categories.append(category)
-        cur.execute(
-            """
-            INSERT INTO meta_cate
-            VALUES (%s, %s)
-            """, (item["asin"], categories.index(category) + 1)
-        )
-    # Insert into feature
-    for feature in item["feature"]:
-        cur.execute(
-            """
-            INSERT INTO feature
-            VALUES (%s, %s)
-            """, (item["asin"], sanitize_string(feature))
-        )
-    # Insert into description
-    for description in item["description"]:
-        cur.execute(
-            """
-            INSERT INTO description
-            VALUES (%s, %s)
-            """, (item["asin"], sanitize_string(description))
-        )
-    # Insert into rank
-    for rank in item["rank"]:
-        cur.execute(
-            """
-            INSERT INTO rank
-            VALUES (%s, %s)
-            """, (item["asin"], sanitize_string(rank))
-        )
+    # # Insert into also_buy
+    # for buy_asin in item["also_buy"]:
+    #     cur.execute(
+    #         """
+    #         INSERT INTO also_buy
+    #         VALUES (%s, %s)
+    #         """, (item["asin"], buy_asin)
+    #     )
+    # # Insert into also_view
+    # for view_asin in item["also_view"]:
+    #     cur.execute(
+    #         """
+    #         INSERT INTO also_view
+    #         VALUES (%s, %s)
+    #         """, (item["asin"], view_asin)
+    #     )
+    # # Insert into category and meta_cate
+    # for category in item["category"]:
+    #     if category not in categories:
+    #         cur.execute(
+    #             """
+    #             INSERT INTO category
+    #             VALUES (%s, %s)
+    #             """, (len(category) + 1, sanitize_string(category))
+    #         )
+    #         categories.append(category)
+    #     cur.execute(
+    #         """
+    #         INSERT INTO meta_cate
+    #         VALUES (%s, %s)
+    #         """, (item["asin"], categories.index(category) + 1)
+    #     )
+    # # Insert into feature
+    # for feature in item["feature"]:
+    #     cur.execute(
+    #         """
+    #         INSERT INTO feature
+    #         VALUES (%s, %s)
+    #         """, (item["asin"], sanitize_string(feature))
+    #     )
+    # # Insert into description
+    # for description in item["description"]:
+    #     cur.execute(
+    #         """
+    #         INSERT INTO description
+    #         VALUES (%s, %s)
+    #         """, (item["asin"], sanitize_string(description))
+    #     )
+    # # Insert into rank
+    # for rank in item["rank"]:
+    #     cur.execute(
+    #         """
+    #         INSERT INTO rank
+    #         VALUES (%s, %s)
+    #         """, (item["asin"], sanitize_string(rank))
+    #     )
 conn.commit()
 print("Inserting Done")
--- utils.py
+++ utils.py
@@ -2,3 +2,11 @@ def sanitize_string(input_string):
     # 将字符串中的 NUL 字符替换为其他字符（这里使用空格）
     sanitized_string = input_string.replace('\x00', ' ')
     return sanitized_string
+
+
+def isfloat(value):
+    try:
+        float_value = float(value)
+        return True
+    except ValueError:
+        return False
