bffe976f3b6d18a4a283b8b953d966f2e6a91b86
cher <cher18168402859@163.com>
2023-11-17 09:32:52 +0800

    update

--- metadata.py
+++ metadata.py
@@ -1,12 +1,13 @@
 import psycopg2
 import json
+import rich.progress
 
 file_path = "/home/yc/dataset/amazon/Sports_and_Outdoors.json"
 
 print("Reading Data ...")
 data = []
 with open(file_path, 'r') as f:
-    for line in f.readlines():
+    for line in rich.progress.track(f.readlines()):
         data.append(json.loads(line))
 
 print(len(data))
